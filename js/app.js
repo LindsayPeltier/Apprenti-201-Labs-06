1e716bba6bddb0ae1cb9adc76ee63f1556b79d33
LindsayPeltier <lindsaypeltier@gmail.com>
2019-10-03 15:10:02 -0700

    object literals

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-''
+'use stict';
+
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+var seattle = {
+  location: 'Seattle',
+  minCustHr: '23',
+  maxCustHr: '65',
+  avgCookCust: '6.3',
+};
+
+console.log(seattle.maxCustHr);
