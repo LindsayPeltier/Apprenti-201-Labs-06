2d5e69b2b616fbc12ae1bfaa36c900f392bb6577
LindsayPeltier <lindsaypeltier@gmail.com>
2019-10-12 17:44:19 -0700

    randomizer and estimated cookies per hour

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-//global variable
+//global variables
 var storeHours = ['6am','7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 //object literals
@@ -8,19 +8,33 @@ var seattle = {
   minCustomerHr: 23,
   maxCustomerHr: 65,
   avgCookieCust: 6.3,
-  randomCustomerHr: function(min, max) {
-    min = Math.ceil(this.minCustomerHr);
-    max = Math.floor(this.maxCustomerHr); 
-    return Math.floor(Math.random() * (max - min +1)) + min;
-  }
+  randomCustomerHr() {
+    return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr) + this.minCustomerHr)
+  },
+  estimatedCookiesHr() {
+    var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  },
 };
-console.log(randomCustomerHr);
 
 var tokyo = {
   location: 'Tokyo',
   minCustomerHr: 3,
   maxCustomerHr: 24,
   avgCookieCust: 1.2,
+  randomCustomerHr() {
+    return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr) + this.minCustomerHr)
+  },
+  estimatedCookiesHr() {
+    var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  },
 };
 
 var dubai = {
@@ -28,6 +42,16 @@ var dubai = {
   minCustomerHr: 11,
   maxCustomerHr: 38,
   avgCookieCust: 3.7,
+  randomCustomerHr() {
+    return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr) + this.minCustomerHr)
+  },
+  estimatedCookiesHr() {
+    var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  },
 };
 
 var paris = {
@@ -35,6 +59,16 @@ var paris = {
   minCustomerHr: 20,
   maxCustomerHr: 38,
   avgCookieCust: 2.3,
+  randomCustomerHr() {
+    return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr) + this.minCustomerHr)
+  },
+  estimatedCookiesHr() {
+    var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  },
 };
 
 var lima = {
@@ -42,5 +76,19 @@ var lima = {
   minCustomerHr: 2,
   maxCustomerHr: 16,
   avgCookieCust: 4.6,
+  randomCustomerHr() {
+    return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr) + this.minCustomerHr)
+  },
+  estimatedCookiesHr() {
+    var result = [];
+    for (var i = 0; i < storeHours.length; i++){
+      result.push(Math.floor(this.randomCustomerHr() * this.avgCookieCust));
+    }
+  return result;
+  },
 };
 
+
+//Display the values of each array as unordered lists in the browser
+
+//Display the lists on sales.html. 
